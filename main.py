365bd775ec636135f8e8ab979eef6304f424dfd2
git-since2005 <84366921+git-since2005@users.noreply.github.com>
2025-03-31 08:04:55 +0530

    IP update

--- main.py
+++ main.py
@@ -307,4 +307,4 @@ def userData():
         return jsonify({"status":"server"}), 500
 
 if __name__ == "__main__":
-    app.run(debug=True, host="127.0.0.1", port=8000)
+    app.run(debug=True, host="0.0.0.0", port=8000)
